42e02fa934e33cb2daecca6fa35b9c643ad3fcb7
Alejandro VIlla <alexandros17.sk@gmail.com>
2020-12-29 07:17:50 -0600

    preparing the folder structure for redux advance

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -8,7 +8,7 @@ import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import Spinner from '../../components/UI/Spinner/Spinner';
 import withErrorHandler from '../withErrorHandler/withErrorHandler';
-import * as actionTypes from '../../store/actions';
+import * as actionTypes from '../../store/actions/actionTypes';
 
 
 const BurgerBuilder = (props) => {
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 
 import * as serviceWorker from './serviceWorker';
-import reducer from './store/reducer';
+import reducer from './store/reducers/burgerBuilder';
 import App from './App';
 
 import './index.css';
