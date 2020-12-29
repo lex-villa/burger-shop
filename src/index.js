7ed0c207b57e10e30d65c67a6159807ffcca3a45
Alejandro VIlla <alexandros17.sk@gmail.com>
2020-12-29 07:07:48 -0600

    redux devtools added

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import App from './App';
 import './index.css';
 
 
-const store = createStore(reducer);
+const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(
   <React.StrictMode>
