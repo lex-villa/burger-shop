5e7bf3377ef3ba08a03990bcccaa930f03811f93
Alejandro VIlla <alexandros17.sk@gmail.com>
2021-01-02 16:40:12 -0600

    lazy loading with React.Lazy() added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import Layout from './containers/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
-import Checkout from './containers/Checkout/Checkout';
-import Orders from './containers/Orders/Orders';
-
 import './App.css';
+
+const Orders = React.lazy(() => {
+  return import('./containers/Orders/Orders');
+});
+const Checkout = React.lazy(() => {
+  return import('./containers/Checkout/Checkout');
+});
+//import Checkout from './containers/Checkout/Checkout';
+//import Orders from './containers/Orders/Orders';
+
 
 function App() {
   return (
     <div>
       <Layout>
-        <Switch>
-          <Route path='/checkout' component={Checkout} />
-          <Route path='/orders' component={Orders} />
-          <Route path='/' component={BurgerBuilder} />
-        </Switch>
+        <Suspense fallback={<p>Loading...</p>}>
+          <Switch>
+            <Route path='/checkout' render={() => <Checkout />} />
+            <Route path='/orders' render={() => <Orders />} />
+            <Route path='/' component={BurgerBuilder} />
+          </Switch>
+        </Suspense>
       </Layout>
     </div>
   );
