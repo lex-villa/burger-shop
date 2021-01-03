87ef00910db08f7af0ebd3c0c574e4a7a4f6a505
Alejandro VIlla <alexandros17.sk@gmail.com>
2021-01-02 18:12:11 -0600

    use of hooks fixed

--- src/containers/withErrorHandler/withErrorHandler.js
+++ src/containers/withErrorHandler/withErrorHandler.js
@@ -6,22 +6,21 @@ const withErrorHandler = (WrappedComponent, axios) => {
     return (props) => {
         const [error, setError] = useState(null);
 
+        const reqInterceptor = axios.interceptors.request.use(req => {
+            setError(null);
+            return req;
+        });
+
+        const resInterceptor = axios.interceptors.response.use(res => res, err => {
+            setError(err);
+        });
+
         useEffect(() => {
-            const reqInterceptor = axios.interceptors.request.use(req => {
-                setError(null);
-                return req;
-            });
-
-            const resInterceptor = axios.interceptors.response.use(res => res, err => {
-                setError(err);
-            });
-
             return(() => {
                 axios.interceptors.request.eject(reqInterceptor);
                 axios.interceptors.response.eject(resInterceptor);
             });
-
-        }, []);
+        }, [reqInterceptor, resInterceptor]);
 
         const errorConfirmedHandler = () => {
             setError(null);
