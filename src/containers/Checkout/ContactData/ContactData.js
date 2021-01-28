fc5d936ded1411d98a6e4bc917cc1f69257bb6fd
Jhovanny Alejandro Bahena Villasenor <jhovanny.bahena@globant.com>
2021-01-28 10:01:43 -0600

    sharing the validation model

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -6,6 +6,7 @@ import Button from '../../../components/UI/Button/Button';
 import Spinner from '../../../components/UI/Spinner/Spinner';
 import Input from '../../../components/UI/Input/Input';
 import withErrorHandler from '../../../containers/withErrorHandler/withErrorHandler';
+import { checkValidity } from '../../../shared/utility';
 import * as actions from '../../../store/actions/index';
 
 import './ContactData.css';
@@ -118,28 +119,6 @@ const ContactData = (props) => {
         props.onOrderBurger(order, props.token);
     };
 
-    const checkValidity = (value, rules) => {
-        let isValid = true;
-
-        if (!rules) {
-            return true;
-        };
-
-        if (rules.required) {
-            isValid = value.trim() !== '' && isValid;
-        };
-
-        if (rules.minLength) {
-            isValid = value.length >= rules.minLength && isValid;
-        };
-
-        if (rules.maxLength) {
-            isValid = value.length <= rules.maxLength && isValid;
-        };
-
-        return isValid;
-
-    };
 
     const inputChangedHandler = (event, inputIdentifier) => {
         const updatedOrderForm = {
