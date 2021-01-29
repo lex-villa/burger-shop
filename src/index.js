126201cc5a33a8ca5b760b779247d371ec8206f1
Jhovanny Alejandro Bahena Villasenor <jhovanny.bahena@globant.com>
2021-01-29 10:14:24 -0600

    logic of orders into a Saga

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import createSagaMiddleware from 'redux-saga';
 import burgerbuilderReducer from './store/reducers/burgerBuilder';
 import orderReducer from './store/reducers/order';
 import authReducer from './store/reducers/auth';
-import { watchAuth, watchBurgerBuilder } from './store/sagas/index';
+import { watchAuth, watchBurgerBuilder, watchOrder } from './store/sagas/index';
 import App from './App';
 
 import './index.css';
@@ -31,6 +31,7 @@ const store = createStore(rootReducer, composeEnhancers(
 
 sagaMiddleware.run(watchAuth);
 sagaMiddleware.run(watchBurgerBuilder);
+sagaMiddleware.run(watchOrder);
 
 ReactDOM.render(
   // <React.StrictMode>
--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -3,6 +3,7 @@ import { takeEvery } from 'redux-saga/effects';
 import * as actionTypes from '../actions/actionTypes';
 import { logoutSaga, checkAuthTimeOutSaga, authUserSaga, authCheckStateSaga } from './auth';
 import { initIngredientsSaga } from './burgerBuilder';
+import { purchaseBurgerSaga, fetchOrdersSaga } from './order';
 
 
 export function* watchAuth() {
@@ -14,4 +15,9 @@ export function* watchAuth() {
 
 export function* watchBurgerBuilder() {
     yield takeEvery(actionTypes.INIT_INGREDIENTS, initIngredientsSaga);
+};
+
+export function* watchOrder() {
+    yield takeEvery(actionTypes.PURCHASE_BURGER, purchaseBurgerSaga);
+    yield takeEvery(actionTypes.FETCH_ORDERS, fetchOrdersSaga);
 };
